5fdb57b21cf11dbb5049db092e315d2e670f15c4
fuguozhu <fuguozhu1994@gmail.com>
2025-09-10 22:43:07 +0800

    Delete todo-backend directory

--- todo-backend/src/main/java/com/example/todo/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.todo;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-    List<Todo> findByUser(User user); // ユーザーごとのToDo取得
-}
